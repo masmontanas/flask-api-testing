9d47942f59d87a78a2bcc78147b45338be99f7c3
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-02 19:30:08 -0800

    added task endpoint and preliminary test

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 TASKS = {'task1':{'task':'blah'}, 'task2':{'task':'blah1'}}
 
+def abort_if_task_doesnt_exist(task_id):
+    if task_id not in TASKS:
+        abort(404, message="Task {} doesn't exist".format(task_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
 class TaskList(Resource):
     def get(self):
         return TASKS
+
+class Task(Resource):
+    def get(self, task_id):
+        abort_if_task_doesnt_exist(task_id)
+        return TASKS[task_id]
 
 class HelloWorld(Resource):
     def get(self):
@@ -16,6 +28,7 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(TaskList, '/tasklist')
+api.add_resource(Task, '/task/<task_id>')
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -17,6 +17,10 @@ class TestCase(unittest.TestCase):
         response = self.app.get('/tasklist', follow_redirects = True)
         self.assertEqual(response.status_code, 200)
 
+    def test_task_get(self):
+        response = self.app.get('/task/task1', follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
 if __name__ == '__main__':
     try:
         unittest.main()
