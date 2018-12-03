4d8b8fd75c07243d171d7b81af1c841ab53dc31e
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-02 19:10:31 -0800

    getting going with tasklist resource

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+TASKS = {'task1':{'task':'blah'}, 'task2':{'task':'blah1'}}
+
+class TaskList(Resource):
+    def get(self):
+        return TASKS
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(TaskList, '/tasklist')
 
 if __name__ == '__main__':
     app.run()
