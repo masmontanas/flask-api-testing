e30a5a2453ac03dce9337a46517f1c2119503cf2
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-02 23:30:02 -0800

    adding post method for new tasks

--- app.py
+++ app.py
@@ -17,18 +17,27 @@ class TaskList(Resource):
     def get(self):
         return TASKS
 
+    def post(self):
+        args = parser.parse_args()
+        task_id = int(max(TASKS.keys()).lstrip('task')) + 1
+        task_id = 'task%i' % task_id
+        TASKS[task_id] = {'task': args['task']}
+        return TASKS[task_id], 201
+
 class Task(Resource):
     def get(self, task_id):
         abort_if_task_doesnt_exist(task_id)
         return TASKS[task_id]
+
+
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
-api.add_resource(TaskList, '/tasklist')
-api.add_resource(Task, '/task/<task_id>')
+api.add_resource(TaskList, '/tasks')
+api.add_resource(Task, '/tasks/<task_id>')
 
 if __name__ == '__main__':
     app.run()
